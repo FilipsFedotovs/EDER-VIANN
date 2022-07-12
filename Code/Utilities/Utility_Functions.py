3e22df52948be07915bdf323ee5b6306c70b2784
Leah Wolf <leahwolf@eduroam-int-dhcp-97-44-208.ucl.ac.uk>
2022-07-12 14:54:34 +0100

    switched off transverse rotation

--- Code/Utilities/Utility_Functions.py
+++ Code/Utilities/Utility_Functions.py
@@ -419,41 +419,41 @@ class Seed:
 #                __hits[2]=(__Z*math.cos(-__Angle)) - (__Pos * math.sin(-__Angle))
 #                __hits[0]=(__Z*math.sin(-__Angle)) + (__Pos * math.cos(-__Angle))
           #Lon Rotate y
-          __LongestDistance=0.0
-          __Track=__TempTrack[self.LongestTrackInd]
-          __Vardiff=float(__Track[len(__Track)-1][1])
-          __Zdiff=float(__Track[len(__Track)-1][2])
-          __vector_1 = [__Zdiff, 0]
-          __vector_2 = [__Zdiff, __Vardiff]
-          __Angle=Seed.angle_between(__vector_1, __vector_2)
-          if np.isnan(__Angle)==True:
-                    __Angle=0.0
-          for __Tracks in __TempTrack:
-           for __hits in __Tracks:
-                __Z=float(__hits[2])
-                __Pos=float(__hits[1])
-                __hits[2]=(__Z*math.cos(-__Angle)) - (__Pos * math.sin(-__Angle))
-                __hits[1]=(__Z*math.sin(-__Angle)) + (__Pos * math.cos(-__Angle))
+#          __LongestDistance=0.0
+#          __Track=__TempTrack[self.LongestTrackInd]
+#          __Vardiff=float(__Track[len(__Track)-1][1])
+#          __Zdiff=float(__Track[len(__Track)-1][2])
+#          __vector_1 = [__Zdiff, 0]
+#          __vector_2 = [__Zdiff, __Vardiff]
+#          __Angle=Seed.angle_between(__vector_1, __vector_2)
+#          if np.isnan(__Angle)==True:
+#                    __Angle=0.0
+#          for __Tracks in __TempTrack:
+#           for __hits in __Tracks:
+#                __Z=float(__hits[2])
+#                __Pos=float(__hits[1])
+#                __hits[2]=(__Z*math.cos(-__Angle)) - (__Pos * math.sin(-__Angle))
+#                __hits[1]=(__Z*math.sin(-__Angle)) + (__Pos * math.cos(-__Angle))
           #Phi rotate print
 
-          __LongestDistance=0.0
-          for __Track in __TempTrack:
-                __X=float(__Track[len(__Track)-1][0])
-                __Y=float(__Track[len(__Track)-1][1])
-                __Distance=math.sqrt((__X**2)+(__Y**2))
-                if __Distance>=__LongestDistance:
-                 __LongestDistance=__Distance
-                 __vector_1 = [__Distance, 0]
-                 __vector_2 = [__X, __Y]
-                 __Angle=-Seed.angle_between(__vector_1,__vector_2)
-          if np.isnan(__Angle)==True:
-                    __Angle=0.0
-          for __Tracks in __TempTrack:
-            for __hits in __Tracks:
-                __X=float(__hits[0])
-                __Y=float(__hits[1])
-                __hits[0]=(__X*math.cos(__Angle)) - (__Y * math.sin(__Angle))
-                __hits[1]=(__X*math.sin(__Angle)) + (__Y * math.cos(__Angle))
+#          __LongestDistance=0.0
+#          for __Track in __TempTrack:
+#                __X=float(__Track[len(__Track)-1][0])
+#                __Y=float(__Track[len(__Track)-1][1])
+#                __Distance=math.sqrt((__X**2)+(__Y**2))
+#                if __Distance>=__LongestDistance:
+#                 __LongestDistance=__Distance
+#                 __vector_1 = [__Distance, 0]
+#                 __vector_2 = [__X, __Y]
+#                 __Angle=-Seed.angle_between(__vector_1,__vector_2)
+#          if np.isnan(__Angle)==True:
+#                    __Angle=0.0
+#          for __Tracks in __TempTrack:
+#            for __hits in __Tracks:
+#                __X=float(__hits[0])
+#                __Y=float(__hits[1])
+#                __hits[0]=(__X*math.cos(__Angle)) - (__Y * math.sin(__Angle))
+#                __hits[1]=(__X*math.sin(__Angle)) + (__Y * math.cos(__Angle))
 
           #After shift
 
