4d2f309d352878ce0a96441abf314d99c8484359
WenqingXie47 <wenqing_xie@126.com>
2022-07-25 13:13:41 +0100

    debug

--- Code/Utilities/Utility_Functions.py
+++ Code/Utilities/Utility_Functions.py
@@ -641,7 +641,7 @@ class Seed:
           
 
           self.GraphSeed=Data(x=torch.Tensor(__graphData_x), 
-                              edge_index = torch.Tensor(__graphData_edge_index, dtype=torch.int8).t().contiguous(),
+                              edge_index = torch.Tensor(__graphData_edge_index).t().contiguous().long(),
                               y=torch.Tensor(__graphData_y))
        
           print(self.GraphSeed.x)
