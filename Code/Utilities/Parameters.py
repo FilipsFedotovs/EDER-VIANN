3f901b95aeef26946c7ab56dbdc40932ab91cc13
Leah Wolf <leahwolf@eduroam-int-dhcp-97-195-143.ucl.ac.uk>
2022-07-28 10:52:17 +0100

    CNN experimenting

--- Code/Utilities/Parameters.py
+++ Code/Utilities/Parameters.py
@@ -143,9 +143,9 @@ if Config=='SND':
     ModelArchitecture=\
         [[1, 4, 1, 2, 2, 2, 2], #Layer 1
             [1, 4, 1, 0, 2, 2, 2], #Layer 2
-            [2, 4, 2, 2, 2, 2, 2], #Layer 3
-            [], #Layer 4
-            [], #Layer 5
+            [1, 4, 1, 0, 2, 2, 2], #Layer 3
+            [1, 4, 2, 0, 2, 2, 2], #Layer 4
+            [2, 4, 2, 2, 2, 2, 2], #Layer 5
             [1, 4, 2], #Dense Layer 1
             [], #Dense Layer 2
             [], #Dense Layer 3
