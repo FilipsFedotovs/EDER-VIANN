73861077fa9bbf54ae24693d5d9db5f1131176e9
Leah Wolf <leahwolf@eduroam-int-dhcp-97-195-143.ucl.ac.uk>
2022-07-29 16:20:10 +0100

    GNN

--- Code/Utilities/GM5_TrainModel_Sub.py
+++ Code/Utilities/GM5_TrainModel_Sub.py
@@ -165,7 +165,9 @@ from torch_geometric.loader import DataLoader
 
 train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
-print(train_loader)
+print(train_dataset[0].x)
+print(train_dataset[0].edge_index)
+print(train_dataset[0])
 
 model = GCN(hidden_channels=4)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
