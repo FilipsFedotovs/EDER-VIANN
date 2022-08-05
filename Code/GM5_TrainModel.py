e4bb48e61dfd4cda5d882dbebd0024c30dd44c8c
Filips <fedotovs@hotmail.com>
2022-08-05 15:19:53 +0100

    Enable continuation of the training on HTCondor

--- Code/GM5_TrainModel.py
+++ Code/GM5_TrainModel.py
@@ -139,7 +139,7 @@ if mode=='C':
    CurrentEpoch=int(PreviousJob[0][1])
    log_name=EOSsubModelDIR+'/'+ PreviousJob[0][5] + '.csv'
    Result=UF.LogOperations(log_name,'ReadLog', '_')
-   print(int(Result[len(Result)]))
+   print(int(Result[-1]))
    exit()
    ###Working out the latest batch
    ###Working out the remaining jobs
