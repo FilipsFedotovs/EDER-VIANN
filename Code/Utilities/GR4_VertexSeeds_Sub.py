f2bfeec45835936e7a23005095453dd1f44bafe1
Filips <fedotovs@hotmail.com>
2022-08-28 17:26:40 +0100

    Add GNN Reconstruction step

--- Code/Utilities/GR4_VertexSeeds_Sub.py
+++ Code/Utilities/GR4_VertexSeeds_Sub.py
@@ -5,6 +5,7 @@ import Utility_Functions as UF
 import argparse
 import pandas as pd #We use Panda for a routine data processing
 import pickle
+import torch
 # import tensorflow as tf
 # from tensorflow import keras
 
@@ -55,8 +56,47 @@ seed_counter=0
 print(UF.TimeStamp(),bcolors.OKGREEN+'Data has been successfully loaded and prepared..'+bcolors.ENDC)
 print(UF.TimeStamp(),'Loading the model...')
 #Load the model
+num_node_features = 4
+num_classes = 2
+class model(torch.nn.Module):
+    def __init__(self, hidden_channels):
+        super(model, self).__init__()
+        torch.manual_seed(12345)
+        #TAGCN layers
+        self.tagconv1 = TAGConv(num_node_features, hidden_channels)
+        self.tagconv2 = TAGConv(hidden_channels, hidden_channels)
+        self.tagconv3 = TAGConv(hidden_channels, hidden_channels)
+
+        self.lin = Linear(hidden_channels, num_classes)
+        self.softmax = Softmax(dim=-1)
+
+    def forward(self, x, edge_index, edge_attr, batch):
+        # 1. Obtain node embeddings
+        #x = self.conv1(x, edge_index)
+        #x = self.tagconv1(x, edge_index)
+        x = self.gmmconv1(x, edge_index, edge_attr)
+        x = x.relu()
+
+        #x = self.conv2(x, edge_index)
+        #x = self.tagconv2(x, edge_index)
+        x = self.gmmconv2(x, edge_index, edge_attr)
+        x = x.relu()
+
+        #x = self.conv3(x, edge_index)
+        #x = self.tagconv3(x, edge_index)
+        x = self.gmmconv3(x, edge_index, edge_attr)
+
+        # 2. Readout layer
+        x = global_mean_pool(x, batch)  # [batch_size, hidden_channels]
+
+        # 3. Apply a final classifier
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.lin(x)
+        x = self.softmax(x)
+        return x
 model_name=EOS_DIR+'/EDER-VIANN/Models/'+args.ModelName
-model=tf.keras.models.load_model(model_name)
+model = model(hidden_channels=16)
+model.load_state_dict(torch.load(model_name))
 #create seeds
 GoodSeeds=[]
 print(UF.TimeStamp(),'Beginning the vertexing part...')
